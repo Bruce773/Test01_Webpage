389d691b0b9d7685a99f15b26f98c47e7a8f1623
Bruce773 <bruce@yogainternational.com>
2017-07-28 09:03:03 -0400

    Fixed the JavaScript clock on the JavaScript page

--- test01_scripts.js
+++ test01_scripts.js
@@ -1,2 +1,17 @@
-var date = new Date();
-document.getElementById("date-container").innerHTML = "<p>Today's date is: " + date + "</p>";
+function runTheClock() {
+    var date = new Date();
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    document.getElementById("date-container").innerHTML = "<h1 style=\"text-align: center;\">Hour: " + hourFix(hr) + " Minute: " + min + " Second: " + sec + "</h1>";
+}
+
+
+    function hourFix(hours) {
+        return hours >= 13 ? (hours - 12) : hours;
+    }
+
+
+
+
+var interval = setInterval(runTheClock, 1000);
