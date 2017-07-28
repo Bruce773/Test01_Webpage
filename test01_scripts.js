40dce0eb69d8f830e08e764d62263ca715b8b384
Bruce773 <bruce@yogainternational.com>
2017-07-28 10:07:28 -0400

    Added a minuteFix class

--- test01_scripts.js
+++ test01_scripts.js
@@ -3,12 +3,16 @@ function runTheClock() {
     let hr = date.getHours();
     let min = date.getMinutes();
     let sec = date.getSeconds();
-    document.getElementById("date-container").innerHTML = "<h5 style=\"text-align: center;\">Time: " + hourFix(hr) + ":" + min + ": " + sec + "</h5>";
+    document.getElementById("date-container").innerHTML = "<h5 style=\"text-align: center;\">Time: " + hourFix(hr) + ":" + minuteFix(min) + ": " + sec + "</h5>";
 }
 
 
 function hourFix(hours) {
     return hours >= 13 ? (hours - 12) : hours;
+}
+
+function minuteFix(min) {
+    return min <= 9 ? ("0" + min) : min;
 }
 
 
